1def1d06d0aa3766fff49757ad076ea1ec66ca95
Yasuto Nishii <yast@me.com>
2024-11-29 21:03:50 +0900

    wip (#9)
    Co-authored-by: Yasuto Nishii <yast@me.comm>

--- core/db_test.go
+++ core/db_test.go
@@ -6,10 +6,12 @@ import (
 	"testing"
 )
 
-const testDBFile = "test_db.db"
-
 func setupTestDB() {
-	os.Remove(testDBFile)
+	// 66 sec if open and close file per write
+	for i := 0; i < numOfSegments; i++ {
+		dbFiles[i] = fmt.Sprintf("%s%d%s", dbPrefix, i, dbSuffix)
+		os.Remove(dbFiles[i])
+	}
 
 	Set("key1", "value1")
 	Set("key2", "value2")
@@ -24,6 +26,18 @@ func setupTestDB() {
 		Set(key, val)
 	}
 
+	for i := 10; i < 1000000; i++ {
+		key := fmt.Sprintf("key%d", i)
+		val := fmt.Sprintf("value%dxxx", i)
+		Set(key, val)
+	}
+
+	for i := 10; i < 1000000; i++ {
+		key := fmt.Sprintf("key%d", i)
+		val := fmt.Sprintf("value%dzzz", i)
+		Set(key, val)
+	}
+
 }
 
 func teardownTestDB() {
@@ -34,8 +48,6 @@ func TestGet(t *testing.T) {
 	// 50000 keys / sec
 	// 2.4 sec with memory index
 	// 0.48 sec with initialize from memory
-
-	dbFile = testDBFile
 
 	setupTestDB()
 	defer teardownTestDB()
@@ -56,12 +68,12 @@ func TestGet(t *testing.T) {
 		{"key2", "hoge"},
 		{"key3", "value3"},
 		{"key4", "key not found: key4"},
-		{"key10", "value10"},
-		{"key100", "value100"},
-		{"key110", "value110"},
-		{"key110", "value110"},
-		{"key120", "value120"},
-		{"key1000", "value1000"},
+		{"key10", "value10zzz"},
+		{"key100", "value100zzz"},
+		{"key110", "value110zzz"},
+		{"key110", "value110zzz"},
+		{"key120", "value120zzz"},
+		{"key1000", "value1000zzz"},
 	}
 
 	for _, tt := range tests {
