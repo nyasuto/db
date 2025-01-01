bd4405b94bfad95df8bced53a3ffb64939944c27
Yasuto Nishii <yast@me.com>
2025-01-01 22:16:53 +0900

    merge feature

--- core/segment_test.go
+++ core/segment_test.go
@@ -22,10 +22,28 @@ func TestRead(t *testing.T) {
 	//  10k: 0.614sec, 16k read/sec
 	// 100k: 0.766sec, 130k read/sec
 	//   1m: 2.196sec, 454k read/sec
+	testSize := 1000 * 1000
 
-	for i := 1; i <= 1000*1000; i++ {
+	for i := 1; i <= testSize; i++ {
 		key := "key" + strconv.Itoa(i)
 		value := "value" + strconv.Itoa(i)
+
+		val, err := manager.Read(key)
+		assert.Nil(t, err)
+		assert.Equal(t, value, val)
+	}
+
+	for i := 1; i <= testSize; i++ {
+		key := "key" + strconv.Itoa(i)
+		value := "value" + strconv.Itoa(i + 10000)
+
+		err := manager.Write(key, value)
+		assert.Nil(t, err)
+	}
+
+	for i := 1; i <= testSize; i++ {
+		key := "key" + strconv.Itoa(i)
+		value := "value" + strconv.Itoa(i + 10000)
 
 		val, err := manager.Read(key)
 		assert.Nil(t, err)
