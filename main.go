2124235b2e40f7730569ff9eafb0573e402c58b3
Yasuto Nishii <yast@me.com>
2025-01-01 21:48:19 +0900

    remove  duplication code (#22)

--- main.go
+++ main.go
@@ -1,25 +1,5 @@
 package main
 
-import (
-	"fmt"
-
-	db "github.com/nyasuto/db/core"
-)
-
 func main() {
-	
-	err := db.Init()
-	if err != nil {
-		fmt.Println("Error in Init:", err)
-		return
-	}
-	db.Set("key1", "value1")
-	ret, err := db.Get("key1")
-
-	if err != nil {
-		fmt.Println("Error reading file:", err)
-		return
-	}
-	fmt.Print(ret)
-
+	// This is a placeholder for the main function
 }
