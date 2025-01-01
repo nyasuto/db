6ba59007387696cc657f798954c988b81e62e02a
Yasuto Nishii <yast@me.com>
2025-01-01 21:17:14 +0900

    improve segment logic (#21)

--- core/segment_test.go
+++ core/segment_test.go
@@ -10,31 +10,20 @@ import (
 )
 
 func TestRead(t *testing.T) {
-	Write()
+	manager := Write()
 
-	manager, err := NewDefaultSegmentManager()
+	if manager == nil {
+		manager, _ = NewDefaultSegmentManager()
+	}
 
-	assert.Nil(t, err)
 	assert.NotNil(t, manager)
 
-	// 単純な実装　セグメントを一個ずつヒットするまで読む
-	// 100 : 10sec, 10 read/sec
-	// 1000	: 99sec, 10 read/sec
-
 	// インデックス実装版 no offset
-	// 100: 0.833sec, 120 read/sec
-	// 1000: 2.894sec, 345 read/sec
-	// 10000: 23.930sec, 418 read/sec
+	//  10k: 0.614sec, 16k read/sec
+	// 100k: 0.766sec, 130k read/sec
+	//   1m: 2.196sec, 454k read/sec
 
-	// インデックス実装版 with offset
-	// バイトオフセットを使えないのでファイルの中のキーの位置によって性能が大激変する
-	// 100: 0.804sec, 124 read/sec
-	// 1000: 2.867sec, 349 read/sec
-	// 10000: 23sec, 434 read/sec
-	// offset := 964662
-	for i := 1; i <= 10000; i++ {
-		//key := "key" + strconv.Itoa(offset-i)
-		//value := "value" + strconv.Itoa(offset-i)
+	for i := 1; i <= 100*1000; i++ {
 		key := "key" + strconv.Itoa(i)
 		value := "value" + strconv.Itoa(i)
 
@@ -44,15 +33,15 @@ func TestRead(t *testing.T) {
 	}
 }
 
-func Write() {
+func Write() *SegmentManager {
 	_, err := os.Stat(dir)
 	if err == nil {
 		files, err := os.ReadDir(dir)
 		if err != nil {
-			return
+			return nil
 		}
 		if len(files) != 0 {
-			return
+			return nil
 		}
 	}
 	// Ensure the directory exists
@@ -60,11 +49,12 @@ func Write() {
 
 	manager, err := NewSegmentManager(dir, maxSize)
 	if err != nil {
-		return
+		return nil
 	}
 
 	// Write data
-	for i := 1; i <= 1000000; i++ {
+	// 10 M
+	for i := 1; i <= 1*1000*1000; i++ {
 		key := "key" + strconv.Itoa(i)
 		value := "value" + strconv.Itoa(i)
 		err := manager.Write(key, value)
@@ -72,5 +62,5 @@ func Write() {
 			fmt.Println("Error writing to segment:", err)
 		}
 	}
-
+	return manager
 }
